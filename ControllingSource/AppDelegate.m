796e5874de32f5339d97574f2821b3cca15a68eb
andy <yzymonster@gmail.com>
2014-08-21 22:34:32 +0800

    test

--- ControllingSource/AppDelegate.m
+++ ControllingSource/AppDelegate.m
@@ -143,6 +143,7 @@
 // Returns the URL to the application's Documents directory.
 - (NSURL *)applicationDocumentsDirectory {
     return [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject];
+    //test on Refactor
 }
 
 @end
